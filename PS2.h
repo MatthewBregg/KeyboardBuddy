e0386a5fdefc4ef4de84dbd58e1007443d5ade66
Matthew Bregg <bregg@google.com>
2021-06-18 11:28:40 -0500

    Add the license header

--- PS2.h
+++ PS2.h
@@ -1,8 +1,22 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * The PS/2 to USB keyboard converter based on DorianRudolph's work
  * @see https://gist.github.com/DorianRudolph/ca283dfdfd185bc812b7
- * 
- * MIT licensed: https://screenshot.googleplex.com/aT7TCreYpRr
  *
  */
 
